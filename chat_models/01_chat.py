2309a8e32f9daa638326f936b49c3c653360c83f
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-09-23 00:57:16 +0530

    Add google gemini chat model integration

--- chat_models/01_chat.py
+++ chat_models/01_chat.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-model = ChatGoogleGenerativeAI(model="gemini-3.6-flash",
-                               temperature=0.8)
+model = ChatGoogleGenerativeAI(
+    model="gemini-3.6-flash",
+    temperature=0.8)
 
-response = model.invoke("Write a poem about AI?")
+response = model.invoke("Tell me everything about Taj Mahal?")
 
 print(response.content)
