a321d5760e9b2ad265ae5439b78b6634ed5bdc61
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-09-22 12:27:28 +0530

    Add google gemini chat model integration

--- chat_models/chat.py
+++ chat_models/chat.py
@@ -1,16 +1,13 @@
-# To load APIs from environment variable
-
-# from langchain.chat_models import init_chat_model
-# from langchain_mistralai import ChatMistralAI
 from langchain_google_genai import ChatGoogleGenerativeAI
-import os
 
+# To load APIs from environment variable
 from dotenv import load_dotenv
-
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model = "gemini-3.6-flash")
 
-response = model.invoke("What is Agentic AI?")
+model = ChatGoogleGenerativeAI(model="gemini-3.6-flash",
+                               temperature=0.8)
+
+response = model.invoke("Write a poem about AI?")
 
 print(response.content)
