9c4ae7dc4368cdf42ac02fd94a4824201fd59a12
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-09-23 17:27:18 +0530

    Build a chat assistant

--- chat_models/chatbot.py
+++ chat_models/chatbot.py
@@ -1,31 +1,35 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 from dotenv import load_dotenv
 load_dotenv()
 
-
 def getllm():
-   return HuggingFacePipeline(repo_id="openai/gpt-oss-20b")
+   llm = HuggingFaceEndpoint(
+       repo_id="meta-llama/Llama-3.1-8B-Instruct",
+       task="text-generation",
+       temperature=0.2
+   )
 
-def get_model():
-    llm = getllm()
-    return ChatHuggingFace(
-        llm = llm
-    )
+   return ChatHuggingFace(llm=llm)
+
 
 def main():
+
+    messages = []
     print("-----------------Welcome -----------------  ")
     print("\n\nType exit or quit to close the chat!")
 
-    print("What's in you mind")
+    print("What's in your mind")
+    chat_model = getllm()
 
     while True:
         prompt = input("You: ")
+        messages.append(prompt)
         if prompt == "exit" or prompt == "quit":
             break
 
-        chat_model = get_model()
-        response = chat_model.invoke(prompt)
-        print(f"Bot: {(response.content[0]["text"])}")
+        response = chat_model.invoke(messages)
+        messages.append(response)
+        print(f"Bot: {response.content}")
 
 main()
