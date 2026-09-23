c263449e228478fc18571c3dffa7a04e87b7b595
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-09-23 19:18:55 +0530

    Add LangChain messages to store conversation history

--- chat_models/chatbot.py
+++ chat_models/chatbot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,7 +16,18 @@ def getllm():
 
 def main():
 
-    messages = []
+    messages = [
+
+        SystemMessage(content="""
+            You are a helpful AI assistant.
+            Your job is to answer the user's questions clearly, accurately, and in simple language.
+            Give direct answers and avoid unnecessary information.
+            For technical questions, explain concepts with simple examples when useful.
+            If you are unsure about something, clearly say that you are unsure instead of making up information.
+            Maintain a friendly and professional tone.
+            """
+        )
+    ]
     print("-----------------Welcome -----------------  ")
     print("\n\nType exit or quit to close the chat!")
 
@@ -24,12 +36,15 @@ def main():
 
     while True:
         prompt = input("You: ")
-        messages.append(prompt)
+        messages.append(HumanMessage(content=prompt))
         if prompt == "exit" or prompt == "quit":
             break
 
         response = chat_model.invoke(messages)
-        messages.append(response)
+        messages.append(AIMessage(content=response.content))
         print(f"Bot: {response.content}")
+
+    print(f"__________________________________"
+          f"\n\n\n{messages}")
 
 main()
